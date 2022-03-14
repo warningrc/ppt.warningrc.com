f3dedfa72a9e70d3fb34b9ab38ac99ee684fd085
王宁 <wangning01@koolearn.com>
2022-03-14 18:08:47 +0800

    x

--- public/aliyun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf ./publish
-
-nodeppt release
-
-rm -rf ./publish/.git
-
-rsync -artvz --delete ./publish/ aliyun:/data/webroot/ppt/
--- public/js/ga.js
+++
@@ -1,2 +0,0 @@
-document.write('<script async type="text/javascript" src="https://www.googletagmanager.com/gtag/js?id=UA-122225193-2"></script>');
-document.write('<script>\nwindow.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag("js", new Date());\ngtag("config", "UA-122225193-2");</script>');
